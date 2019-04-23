ec4c4103fa75abb77487197869ba6fff8533b0c0
erinkayb <erinkayb@gmail.com>
2019-04-23 11:48:17 +0200

    first commit

---
+++ InventoryServiceApp/Controllers/InventoryController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InventoryServiceApp.Models;
+using InventoryServiceApp.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryServiceApp.Controllers
+{
+    [Route("v1/")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        private readonly IInventoryServices _services;
+
+        public InventoryController(IInventoryServices services)
+        {
+            _services = services;
+        }
+
+        [HttpPost]
+        [Route("AddInventoryItems")]
+        public ActionResult<InventoryItems> AddInventoryItems(InventoryItems items)
+        {
+            var inventoryItems = _services.AddInventoryItems(items);
+
+            if(inventoryItems == null)
+            {
+                return NotFound();
+            }
+            return inventoryItems;
+        }
+
+        [HttpGet]
+        [Route("GetInventoryItems")]
+        public ActionResult<Dictionary<string, InventoryItems>> GetInventoryItems()
+        {
+            var inventoryItems = _services.GetInventoryItems();
+
+            if(inventoryItems.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return inventoryItems;
+        }
+    }
+    
+}
---
+++ InventoryServiceApp/Services/IInventoryServices.cs
@@ -0,0 +1,14 @@
+﻿using InventoryServiceApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryServiceApp.Services
+{
+    public interface IInventoryServices
+    {
+        InventoryItems AddInventoryItems(InventoryItems items);
+        Dictionary<string, InventoryItems> GetInventoryItems();
+    }
+}
